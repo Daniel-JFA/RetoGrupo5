dae67a01d3623c179fb2bcce004b523cc832a342
jaime <jandres.vr92@gmail.com>
2024-07-12 19:18:58 -0500

    feature: dashboard inicio

---
+++ angular/estramipyme/src/app/landing-page/landing.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IntroComponent } from './intro/intro.component';
+import { FeaturesComponent } from './features/features.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@Component({
+  selector: 'landing-page',
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    IntroComponent,
+    FeaturesComponent,
+    TestimonialsComponent,
+    FooterComponent,
+  ],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss'
+})
+export class LandingPageComponent {
+
+}
